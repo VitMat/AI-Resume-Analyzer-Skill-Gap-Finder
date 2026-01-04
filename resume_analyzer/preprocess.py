7c48fec74ec8be96370e16aa0edb42e494178e14
Arnav-Mathur07 <arnav2005mathur@gmail.com>
2026-01-04 22:48:54 +0530

    ntlk changes for stopwords download

--- resume_analyzer/preprocess.py
+++ resume_analyzer/preprocess.py
@@ -1,6 +1,8 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+nltk.download('punkt')
 
 stop_words = set(stopwords.words('english'))
 
